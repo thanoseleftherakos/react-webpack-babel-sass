ad0235516eda456740e8d5a3ac936ec1d19b4897
Thanos Eleftherakos <teleftherakos@gmail.com>
2016-09-22 11:46:19 +0300

    add historyApiFallback to support router

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     output: {
         filename: './dist/main.min.js'
     },
+    devServer: {
+        historyApiFallback: true
+    },
     module: {
         loaders: [
             {
